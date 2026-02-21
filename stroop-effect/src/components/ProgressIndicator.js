416814ef6c9f47a14a52cc92b9bca329d9ccd4df
dwijap84 <dwijap84@gmail.com>
2026-02-21 12:14:41 -0700

    updated progress indicator

--- stroop-effect/src/components/ProgressIndicator.js
+++ stroop-effect/src/components/ProgressIndicator.js
@@ -1,13 +1,12 @@
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
-import { ThemeProvider } from '@mui/material/styles';
 
-export default function ProgressIndicator({pointCount}) {
+export default function ProgressIndicator({ pointCount }) {
     const [indication, setIndication] = useState(0);
 
     useEffect(() => {
         if (pointCount % 6 === 0) {
-            setIndication(indication + 1)
+            setIndication(prev => prev + 1)
         }
     }, [indication, pointCount])
 
@@ -18,20 +17,11 @@ export default function ProgressIndicator({pointCount}) {
             width: 100,
             height: 100,
             borderRadius: 1,
-            bgcolor: 'primary.main',
+            backgroundColor: '#f5ef9c',
         }}></Box>)
     }
 
     return (
-        <ThemeProvider
-            theme={{
-                palette: {
-                    primary: {
-                        main: '#edd58f',
-                    },
-                },
-            }}
-        >       {boxes}
-        </ThemeProvider>
+       {boxes}
     )
 }
