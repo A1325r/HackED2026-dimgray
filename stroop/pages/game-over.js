1338286b172d4f0e824fe7f4da196c3d9948e292
A1325r <amberm.contact@gmail.com>
2026-02-21 16:08:22 -0700

    styling and navigation on game-over and removed button on home page

--- stroop/pages/game-over.js
+++ stroop/pages/game-over.js
@@ -1,27 +1,50 @@
 import Navbar from "@/components/Navbar";
+import Link from "next/link";
 
+import Image from "next/image";
+import { Geist, Geist_Mono } from "next/font/google";
+import { useState } from "react";
+
+const geistSans = Geist({
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
+});
+
+// should take in the number of points and time to display
 export default function GameOverPage() {
     return (
-        <div className="homeScreenButtons">
-            <Navbar />
-            <div className='typing-container'><h1>Game Over</h1></div>
-            <p>You got ${ } points in ${ }!</p>
-            <div className='homeScreenButtons'>
-                <RestartButton />
-                <ModeButton />
-            </div>
+        <div
+            className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
+        >
+            <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+                <Navbar />
+                <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
+                    <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Game Over</h1>
+                </div>
+                <p>You got COUNTER points in TIME!</p>
+                <div>
+                    <Link
+                        className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
+                        href="/"
+                        target="_self"
+                        rel="noopener noreferrer"
+                    >
+                        <Image
+                            className="dark:invert"
+                            src="/vercel.svg"
+                            alt="Vercel logomark"
+                            width={16}
+                            height={16}
+                        />
+                        Try Again
+                    </Link>
+                </div>
+            </main>
         </div>
-    );
-}
-
-function RestartButton() {
-    return (
-        <button class name="restartButton">Try Again</button>
-    );
-}
-
-function ModeButton() {
-    return (
-        <button class name="modeButton">Change Game Mode</button>
     );
 }
--- stroop/pages/index.js
+++ stroop/pages/index.js
@@ -19,25 +19,13 @@ export default function Home() {
       className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
     >
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        
         <Navbar />
-        <a
-          className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-          href="/game-over"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          <Image
-            className="dark:invert"
-            src="/vercel.svg"
-            alt="Vercel logomark"
-            width={16}
-            height={16}
-          />
-          Game Over Screen
-        </a>
+
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Select Game Mode</h1>
         </div>
+
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
           <TimerButton />
           <CountdownButton />
@@ -65,7 +53,6 @@ function TimerButton() {
         <p>How many colors can you match in
           67 seconds?  <br /> For every match you get
           wrong, <br /> the timer reduces 6-7 seconds!</p>
-        <button class="startButton">Start Time Trial</button>
         <a
           className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
           href="/time-trial"
