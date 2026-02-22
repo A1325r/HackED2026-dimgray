f7cbd4a0e18b14aae0b540181b6057e22fa7023f
“macyjalberto” <macyjalberto@gmail.com>
2026-02-21 18:23:15 -0700

    Fixed whoopsie

--- stroop/pages/survival-mode.js
+++ stroop/pages/survival-mode.js
@@ -6,13 +6,37 @@ import GameTimer from "../components/gameTimer";
 import ProgressTimeBar from "../components/progressTimeBar";
 
 export default function SurvivalMode() {
-const [gameState, setGameState] = useState(true);
-const [gamesScore, setGameScore] = useState(0);
+    const [gameState, setGameState] = useState(true);
+    const [gamesScore, setGameScore] = useState(0);
+    const [gameTimer, setGameTimer] = useState(6);
+    const { elapsedTime, progress, secondDur, start } = GameTimer(67); // 67 seconds hahahahahahahah
 
-const handleFail = (score) => {
-    setGameState(false);
-    setGameScore(score);
-}
+    const handleFail = (score) => {
+        setGameState(false);
+        setGameScore(score);
+    }
+
+    // starts timer yt
+    useEffect(() => {
+        start();
+    }, []);
+
+    const handlePass = () => {
+        setGameTimer(6);
+    }
+
+    const timerId = setInterval(function() {
+        setGameTimer(gameTimer - 1)
+        if(gameTimer === 0) {
+            setGameState(false);
+            setGameTimer(0)
+        }
+    }, 1000);
+
+    // To stop the timer after, say, 5 seconds:
+    setTimeout(function() {
+        clearInterval(timerId);
+    }, 6000);
     return (
         <div>
             <Navbar />
@@ -20,7 +44,7 @@ const handleFail = (score) => {
             <ProgressTimeBar progress={progress} elapsedTime={elapsedTime} duration={secondDur}/>
             {progress >= 100 && <p className="time-up">Time's Up!</p>}
             <p>This is survival mode</p>
-            <p>{gameTimer}</p>
+            <p>{GameTimer}</p>
             {gameState && <GameBoard failGame={handleFail} contGame={handlePass}></GameBoard>}
             {!gameState && <GameOverPage displayScore={gamesScore}></GameOverPage>}
         </div>
