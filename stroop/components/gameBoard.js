e25fa637dae83c5c21ba65575e866ecc976ca058
dwijap84 <dwijap84@gmail.com>
2026-02-21 22:45:00 -0700

    level one kinda harder?

--- stroop/components/gameBoard.js
+++ stroop/components/gameBoard.js
@@ -2,90 +2,94 @@ import { useState } from "react";
 import { useRouter } from "next/router";
 import GameButton from "./GameButton";
 import ColorText from "./ColorText";
-export default function GameBoard({failGame, contGame, score}) {
-const router = useRouter();
-// const [gameState, setGameState] = useState(gameState);
-//const [gameColors, setGameColors] = useState([]);
-const [targetColor, setTargetColor] = useState('red');
-const [targetText, setTargetText] = useState('blue');
-//const [score, setScore] = useState(0);
+export default function GameBoard({ failGame, contGame, score }) {
+    const router = useRouter();
+    // const [gameState, setGameState] = useState(gameState);
+    //const [gameColors, setGameColors] = useState([]);
+    const [targetColor, setTargetColor] = useState('red');
+    const [targetText, setTargetText] = useState('blue');
+    //const [score, setScore] = useState(0);
 
-const gameColors = [
-    'red',
-    'blue',
-    'green',
-    'yellow',
-    'purple',
-    'white'
-];
+    const gameColors = [
+        'red',
+        'blue',
+        'green',
+        'yellow',
+        'purple',
+        'white'
+    ];
 
-const getRandomNumber = (max) => {
-    return Math.floor(Math.random() * max);
-}
+    const [randomNumber] = useState(() => Math.floor(Math.random() * gameColors.length));
 
-function failure(number) {
-    failGame(number);
-}
 
-function notFail(int) {
-    contGame(int);
-}
-
-const checkColor = (event) => {
-    const buttonText = event.target.innerText;
-    if(buttonText === targetColor) {
-        //setScore(score + 1);
-        //let number = (Math.random(5));
-        //let tempcolor = gameColors.at(number);
-        //setGameColors(props.colors);
-        //console.log(tempcolor);
-        rollTarget();
-        notFail(score + 1);
-        
-    } else {
-        failure(score);
+    const getRandomNumber = (max) => {
+        return Math.floor(Math.random() * max);
     }
-}
 
-const rollTarget = () => {
-    let tempTarget = getRandomNumber(5);
-    setTargetColor(gameColors[tempTarget]);
-    let tempArray = gameColors;
-    tempArray.splice(tempTarget, 1)
-
-    setTargetText(tempArray[getRandomNumber(4)]);
-
-    levelOne();
-}
-
-const getRandomColour = () => {
-    const randomColourIndex = Math.floor(Math.random() * gameColors.length);
-    return gameColors[randomColourIndex];
-}
-
-const levelOne = () => {
-   let colour = getRandomColour();
-    if(score >= 6) {
-        <GameButton color={colour}></GameButton>
+    function failure(number) {
+        failGame(number);
     }
-}
 
-return ( 
-    <div>
-        <link href="https://fonts.cdnfonts.com/css/joystix" rel="stylesheet"></link>
-        <ColorText textcolor={targetColor} text={targetText}/>
-        <p style={{color: targetColor}}>{score}</p>
-        {/* <GameButton onClick={handleClick} color="red" text="blue"></GameButton> */}
-    <ul>
-        {
-            gameColors.map((item, index) => (
-                <li key={index}>
-                     <GameButton click={checkColor} color={item} text={item}></GameButton>  
-                </li>
-            ))
+    function notFail(int) {
+        contGame(int);
+    }
+
+    const checkColor = (event) => {
+        const buttonText = event.target.innerText;
+        if (buttonText === targetColor) {
+            //setScore(score + 1);
+            //let number = (Math.random(5));
+            //let tempcolor = gameColors.at(number);
+            //setGameColors(props.colors);
+            //console.log(tempcolor);
+            rollTarget();
+            notFail(score + 1);
+
+        } else {
+            failure(score);
         }
-    </ul>
-    </div>
+    }
+
+    const rollTarget = () => {
+        let tempTarget = getRandomNumber(5);
+        setTargetColor(gameColors[tempTarget]);
+        let tempArray = gameColors;
+        tempArray.splice(tempTarget, 1)
+
+        setTargetText(tempArray[getRandomNumber(4)]);
+
+        levelOne();
+    }
+
+    const getRandomColour = () => {
+        const randomColourIndex = Math.floor(Math.random() * gameColors.length);
+        return gameColors[randomColourIndex];
+    }
+
+    const levelOne = () => {
+        let colour = getRandomColour();
+        if (score >= 6) {
+            <GameButton color={colour}></GameButton>
+        }
+    }
+
     
-);
+    return (
+        <div>
+            <link href="https://fonts.cdnfonts.com/css/joystix" rel="stylesheet"></link>
+            <ColorText textcolor={targetColor} text={targetText} />
+            <p style={{ color: targetColor }}>{score}</p>
+            {/* <GameButton onClick={handleClick} color="red" text="blue"></GameButton> */}
+            <ul>
+                {
+                    gameColors.map((item, index) => (
+                        <li key={index}>
+                            {score > 6 ? <GameButton click={checkColor} color={randomColourPicker()} text={item}></GameButton> : <GameButton click={checkColor} color={item} text={item}></GameButton>}
+                        </li>
+                    ))
+                }
+            </ul>
+        </div>
+
+    );
 }
