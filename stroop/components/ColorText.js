3e3727448d42f3bf83f6b4b69cb8489b23dae3e1
A1325r <amberm.contact@gmail.com>
2026-02-21 23:42:41 -0700

    update font size

--- stroop/components/ColorText.js
+++ stroop/components/ColorText.js
@@ -1,6 +1,6 @@
 export default function ColorText({textcolor, text}) {
     return (
-        <p style={{ color: textcolor }}>{text}</p>
+        <p style={{ color: textcolor, fontSize: "30px" }}>{text}</p>
     )
 }
 
