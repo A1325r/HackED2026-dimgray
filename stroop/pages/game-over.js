647f942ca0fc03a4dad7918d1d9f4095fd03cc8c
dwijap84 <dwijap84@gmail.com>
2026-02-21 21:23:03 -0700

    time tracking works!

--- stroop/pages/game-over.js
+++ stroop/pages/game-over.js
@@ -16,7 +16,7 @@ const geistMono = Geist_Mono({
 });
 
 // should take in the number of points and time to display
-export default function GameOverPage({displayScore}) {
+export default function GameOverPage({displayScore, time}) {
     return (
         <div
             className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
@@ -25,7 +25,7 @@ export default function GameOverPage({displayScore}) {
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Game Over</h1>
                 </div>
-                <p>You got {displayScore} points in TIME!</p>
+                <p>You got {displayScore} points in {time} seconds!</p>
                 <div>
                     <Link
                         className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
--- stroop/pages/time-trial.js
+++ stroop/pages/time-trial.js
@@ -13,6 +13,18 @@ export default function TimeTrial() {
     const [timeLeft, setTimeLeft] = useState(67); // Timer value
     const [isActive, setIsActive] = useState(false); // To control the timer
     const [deductedTime, setDeductionTime] = useState(0);
+    const [time, setTime] = useState(0);
+    // state to check whether the rounds still going or not
+    const [isRunning, setIsRunning] = useState(true);
+
+    useEffect(() => {
+        let intervalId;
+        if (isRunning) {
+            // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+            intervalId = setInterval(() => setTime(time + 1), 1000);
+        }
+        return () => clearInterval(intervalId);
+    }, [isRunning, time]);
 
     useEffect(() => {
         let timer;
@@ -26,6 +38,7 @@ export default function TimeTrial() {
         } else if (timeLeft <= 0) {
             setGameState(false);
             setTimeLeft(0);
+            setIsRunning(false);
         }
 
         return () => clearTimeout(timer); // Cleanup the timer
@@ -59,7 +72,7 @@ export default function TimeTrial() {
             <p>{GameTimer}</p>
             <p>{timeLeft}</p>
             {gameState && <GameBoard failGame={handleFail} contGame={handlePass} score={gamesScore}></GameBoard>}
-            {!gameState && <GameOverPage displayScore={gamesScore}></GameOverPage>}
+            {!gameState && <GameOverPage displayScore={gamesScore} time={time}></GameOverPage>}
         </div>
     );
 }
