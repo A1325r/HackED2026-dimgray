635e11ec454c6e477ce1b51d0253b33c18ca674e
A1325r <amberm.contact@gmail.com>
2026-02-21 23:01:22 -0700

    formatting on buttons

--- stroop/components/gamebutton.js
+++ stroop/components/gamebutton.js
@@ -1,13 +1,16 @@
 import react from 'react';
 
-export default function GameButton({click, color, bgColor, text}) {
+export default function GameButton({ click, color, bgColor, text }) {
 
     function handleClick(event) {
         click(event);
     }
 
-return (
-    <button className="gameButton" style={{color: color, backgroundColor: bgColor}} onClick={handleClick}>{text}</button>
-)
+    return (
+        <div>
+            <link href="https://fonts.cdnfonts.com/css/joystix" rel="stylesheet"></link>
+            <button className="gameButton" style={{ color: color, backgroundColor: bgColor, fontFamily: "joystix", fontSize: "25px", width: "250px" }} onClick={handleClick}>{text}</button>
+        </div>
+    )
 }
 
