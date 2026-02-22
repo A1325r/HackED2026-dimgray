20a8bb8735b46e4a978bd6d90d59f3b615a55cdf
A1325r <amberm.contact@gmail.com>
2026-02-22 00:35:24 -0700

    cleaning up code

--- stroop/pages/index.js
+++ stroop/pages/index.js
@@ -1,31 +1,18 @@
-import Image from "next/image";
 import Navbar from "@/components/Navbar";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
   return (
-    <div
-      className={'flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'}
-    >
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <link href="https://fonts.cdnfonts.com/css/joystix" rel="stylesheet"></link>
-        <Navbar />
-
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left home">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Select Game Mode</h1>
-        </div>
-
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <TimerButton />
-          <CountdownButton />
-        </div>
-      </main>
+    <div>
+      <link href="https://fonts.cdnfonts.com/css/joystix" rel="stylesheet"></link>
+      <Navbar />
+      <h1>Select Game Mode</h1>
+      <TimerButton />
+      <CountdownButton />
     </div>
   );
 }
-
-
 
 function TimerButton() {
 
@@ -43,14 +30,7 @@ function TimerButton() {
         <p>How many colors can you match in
           67 seconds?  <br /> For every match you get
           wrong, <br /> the timer reduces by 6 seconds!</p>
-        <Link
-          className="startButton flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-          href="/time-trial"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          Start Time Trial
-        </Link>
+        <Link href="/time-trial" target="_self">Start</Link>
       </div>}
     </div>
   );
@@ -70,21 +50,7 @@ function CountdownButton() {
       <button className="modeButtons" name="countdownButton" onClick={handleCountdownClick}>Survival Mode</button>
       {isCountdownActive === true && <div>
         <p>Play until you get the color wrong! <br /> The questions get progressively harder <br /> and the question timer gets shorter as you play.</p>
-        <Link
-          className="startButton flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-          href="/survival-mode"
-          target="_self"
-          rel="noopener noreferrer"
-        >
-          {/* <Image
-            className="dark:invert"
-            src="/vercel.svg"
-            alt="Vercel logomark"
-            width={16}
-            height={16}
-          /> */}
-          Start Survival Mode
-        </Link>
+        <Link href="/survival-mode" target="_self">Start</Link>
       </div>}
     </div>
   );
